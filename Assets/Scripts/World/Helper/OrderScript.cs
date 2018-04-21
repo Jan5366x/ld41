2429d4971d72c476cdbaf008b7d445ae05f773e2
Kormarun <kormarun@gmail.com>
2018-04-21 21:55:48 +0200

    Let there be order

---
+++ Assets/Scripts/World/Helper/OrderScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class OrderScript : MonoBehaviour
+{
+    public float offset;
+
+    public void Awake()
+    {
+        SetPosition();
+    }
+
+    public void Update()
+    {
+        SetPosition();
+    }
+
+    public void SetPosition()
+    {
+        GetComponent<SpriteRenderer>().sortingOrder = (int) -((transform.position.y + offset) * 100);
+    }
+}
