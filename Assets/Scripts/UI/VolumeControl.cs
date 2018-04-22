dd82c1074aa7426cb5c93a095abb20cd914efca6
Jan Schwien <jan5366@gmail.com>
2018-04-22 15:12:46 +0200

    work on main menu

---
+++ Assets/Scripts/UI/VolumeControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+namespace UI
+{
+	public class VolumeControl: MonoBehaviour {
+
+		public Slider VolumeSlider;
+		public AudioMixer VolumeAudio;
+		public void VolumeController()
+		{
+
+			VolumeAudio.SetFloat("Master", VolumeSlider.value);
+		}
+		
+	
+	}
+}
