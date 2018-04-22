3ffccd84011ab3cf36a1a103f30f12d0cad84bba
Kormarun <kormarun@gmail.com>
2018-04-22 23:18:59 +0200

    Starting inventory

---
+++ Assets/Scripts/UI/ViewInventory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ViewInventory : MonoBehaviour
+{
+    public abstract void handleSelectSlot(int slot);
+
+    private Inventory _inventory;
+
+    public Inventory Inventory
+    {
+        get { return _inventory.Copy(); }
+        set { _inventory = value.Copy(); }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
