7a22bda7c716e4c38885a54e9598b3556f63926b
Jan Schwien <jan5366@gmail.com>
2018-04-26 23:03:02 +0200

    added a chest prefab, work on item template inprovements

--- Assets/Scripts/TemplateSkeleton/Item.cs
+++ Assets/Scripts/TemplateSkeleton/Item.cs
@@ -2,15 +2,34 @@
 
 namespace TemplateSkeleton
 {
+    
+    public enum ItemSlot
+    {
+        Head = 0,
+        Armor = 1,
+        LeftHand = 2,
+        RightHand = 3,
+        Pants = 4,
+        Shoes = 5
+    }
+    
     [CreateAssetMenu(fileName = "New Item", menuName = "Template/Item")]
     public class Item : ScriptableObject {
+		 public static float SellModifier = 0.75f;
+
+
         public string ItemName;
-        public Effect ItemEffect;
         public int BasePrice;
-        public Sprite[] Sprites;
-        public float ArmorResistence;
         public Sprite PreviewSmall;
         public Sprite PreviewLarge;
-        public static float SellModifier = 0.75f;
+        public Effect ItemEffect;
+
+        // TODO replace with a easier to use solution since this takes to much time and can cause issues
+        public Sprite[] Sprites;
+        
+        public float ArmorResistence;
+        
+        // TODO implement usage in game
+        public ItemSlot[] PossibleSlots;
     }
 }
